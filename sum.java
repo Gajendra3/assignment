15c1b9b9667101955c2e3950b91ec612e59696e4
Gajendra Ugale <86650178+Gajendra3@users.noreply.github.com>
2023-07-12 19:00:04 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        int sum=0 ,i=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 5 NO. ");
+        int n =5;
+        while(i<n){
+            System.out.println("Enter integer ");
+           sum = sum + input.nextInt();
+           i++;
+        }
+        System.out.println("Summation is "+sum);
+    }
+}
